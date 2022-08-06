2cb22a92d41d1bfd93458a87d0af1031e069fe3d
Floyd Jones <floydtjones@gmail.com>
2022-08-06 14:03:14 -0500

    css

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -2,18 +2,26 @@
   import loginOrRegister from "../stores/auth";
 </script>
 
-<div class="auth">
-  <button class="google">login with Google</button>
-</div>
-<div class="or">or</div>
-<div class="auth">
-  <button on:click={() => ($loginOrRegister = "register")}
-    >create Account</button
-  >
-  <button on:click={() => ($loginOrRegister = "login")}>login</button>
+<div class="authContainer">
+  <div class="auth">
+    <button class="google">login with Google</button>
+  </div>
+  <div class="or">or</div>
+  <div class="auth">
+    <button on:click={() => ($loginOrRegister = "register")}
+      >create Account</button
+    >
+    <button on:click={() => ($loginOrRegister = "login")}>login</button>
+  </div>
 </div>
 
 <style>
+  .authContainer {
+    border: 3px solid #333;
+    width: fit-content;
+    padding-left: 13px;
+    margin: 0 auto;
+  }
   .auth {
     text-align: center;
     display: flex;
