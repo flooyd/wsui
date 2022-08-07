d6c408d1fafcbc367e9fafeec895526a9e263a39
Floyd Jones <floydtjones@gmail.com>
2022-08-06 21:27:12 -0500

    wss

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,7 +2,7 @@ export const data = {
   requests: [
     {
       name: "demo server",
-      ws: "ws://floyds-gigs.herokuapp.com/",
+      ws: "wss://floyds-gigs.herokuapp.com/",
       events: [
         { name: "findAllThings" },
         { name: "createThing" },
